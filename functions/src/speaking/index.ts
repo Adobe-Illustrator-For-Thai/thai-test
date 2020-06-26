eddf10edfcfcb6fd36ef2878f979042d5a9bfa42
plumsirawit <plurm2545@hotmail.com>
2020-06-27 03:18:58 +0700

    fix: add express multipart file parser

--- functions/src/speaking/index.ts
+++ functions/src/speaking/index.ts
@@ -3,7 +3,10 @@ const router = express.Router();
 import word from "./word";
 router.post("/word", word);
 const app = express();
+//@ts-ignore
+import * as fileMiddleware from "express-multipart-file-parser";
 import * as cors from "cors";
 app.use(cors({ origin: true }));
+app.use(fileMiddleware);
 app.use(router);
 export default app;
