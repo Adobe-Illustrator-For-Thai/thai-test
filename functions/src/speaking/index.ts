677b2cb34aa81126c558b4e41ea3cc47b70fde90
plumsirawit <plurm2545@hotmail.com>
2020-06-27 03:13:23 +0700

    fix: change function method from get to post

--- functions/src/speaking/index.ts
+++ functions/src/speaking/index.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 const router = express.Router();
 import word from "./word";
-router.get("/word", word);
+router.post("/word", word);
 const app = express();
 import * as cors from "cors";
 app.use(cors({ origin: true }));
