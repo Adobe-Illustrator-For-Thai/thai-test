410d2bb0489488fc83ada078f8d43738eeca84f2
Ne0EX <magician2120@hotmail.com>
2020-06-27 21:16:49 +0700

    Index page complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,19 +23,21 @@ const Card = ({imageSrc, header, content}: CardProps) => {
             <Image roundedTop="lg" mb="0" src={imageSrc} width="auto" height="100%" objectFit="cover"></Image>
         </CenterFlex>
         <Box m="15px">
-            <Heading fontFamily="Mitr, sans-serif">{header}</Heading>
-            <Text fontFamily="Quark, sans-serif">{content}</Text>
+            <Heading fontFamily="Lato, sans-serif" fontSize="20px">{header}</Heading>
+            <Text padding="5px 0px" fontFamily="Quark, sans-serif" fontSize="16px">{content}</Text>
         </Box>
     </Box>;
 }
 
 const Cards = () => {
     return <Stack isInline>
-        <Box><Card imageSrc="https://cdn.pixabay.com/photo/2012/04/13/00/14/bar-31174_960_720.png" header="Test" content="test content"/></Box>
-        <Box><Card imageSrc="http://202.28.95.5/ortho/myfile/20170216121353_Test-Computer-Key-by-Stuart-Miles.jpg" header="Test" content="test content"/></Box>
-        <Box><Card imageSrc="http://202.28.95.5/ortho/myfile/20170216121353_Test-Computer-Key-by-Stuart-Miles.jpg" header="Test" content="test content"/></Box>
+        <Box padding="0px 8vmin" rounded="lg"><Card imageSrc="https://sites.google.com/site/indochinappk/_/rsrc/1468738929801/manu3/101451241.jpg" header="Learn through real situation" content="To understand more about Thai language, we present you Thai article in your language for understanding Thai's basic."/></Box>
+        <Box padding="0px 8vmin" rounded="lg"><Card imageSrc="https://cdn-cms.pgimgs.com/static/2020/04/Learn-online.jpg" header="Listen Thai's speech from native AI" content="We introduce you new normal of learning Thai by reading Thai's article to speech."/></Box>
+        <Box padding="0px 8vmin" rounded="lg"><Card imageSrc="https://www.omothailand.com/upload/images/59001c222df45.png" header="Speak Thai instead of Tai" content="Don't know how to speech Thai correctly?
+        Don't worry, we have evaluation system for you."/></Box>
     </Stack>
 }
+
 const IndexPage = () => (
     <Layout>
         <SEO title="Learn Thai as Thai style | Home" />
