fa3b9b5c3ec3a140530e069bd82888d95ed72426
plumsirawit <plurm2545@hotmail.com>
2020-06-28 04:13:29 +0700

    feat: cleanup learn

--- src/pages/learn.tsx
+++ src/pages/learn.tsx
@@ -3,13 +3,16 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Links from "../components/links";
 import {Box,Text,Image,Link,Icon} from "@chakra-ui/core";
-const IndexPage = () => (
+import CenterFlex from "../components/CenterFlex";
+const LearnPage = () => (
     <Layout>
         <SEO title="Learn Thai as Thai style | Learn" />
-        <Box margin="50px 10px">
-            <Text fontFamily="Mitr, sans-serif"fontSize="24px" color="#A3A3A3">Learning</Text>
+        <Box flex="1" paddingX={["20vw", "20vw", "20vw", "12vw"]} marginY={["50px", "50px", "50px", "0px"]}>
+            <Text fontFamily="Mitr, sans-serif" fontSize="24px" color="#A3A3A3">Learning</Text>
             <Text padding="10px 0px" fontFamily="Mitr, sans-serif" fontSize="42px">Today, there is no corona virus patient. Returned home 13 more.</Text>
-            <Image src="https://www.thairath.co.th/media/dFQROr7oWzulq5FZUEj8doZ4k64UQfDgsAh9ikImF69u02SMSkaFh3VbsEtc6aRBVct.webp" alt="Covid" height="60vmin"/>
+            <CenterFlex w="100%" marginY="20px">
+                <Image rounded="7vmin" shadow="md" src="https://www.thairath.co.th/media/dFQROr7oWzulq5FZUEj8doZ4k64UQfDgsAh9ikImF69u02SMSkaFh3VbsEtc6aRBVct.webp" alt="Covid" height="40vmin"/>
+            </CenterFlex>
             <Text padding="10px 0px" fontFamily="Lato, sans-serif" fontSize="20px">
             The COVID-19 data center reports that Thailand has not found any income, resulting in the nationwide infection being "0"
 
@@ -21,4 +24,4 @@ At 11:30 am on June 27, 2020, Facebook Fanpage, the data center of COVID-19, rep
     </Layout>
 );
 
-export default IndexPage;
+export default LearnPage;
