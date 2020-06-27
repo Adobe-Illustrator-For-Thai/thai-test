91ade518f6fb95ef65ca800a9c8d2df49e04e66b
plumsirawit <plurm2545@hotmail.com>
2020-06-28 04:52:33 +0700

    feat: clean links UX

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,25 +1,38 @@
 import React from "react";
 import { Link } from "gatsby";
-import {Box, List,ListItem,Button,Text} from "@chakra-ui/core";
-const ItemRender = (props:any) => {
-    return <ListItem display="inline-block" padding="0 10px">
-        <Link to={props.to}>{props.children}</Link>
-        </ListItem>
-}
+import { Box, List, ListItem, Button, Text, Stack, Grid } from "@chakra-ui/core";
+const ItemRender = (props: any) => {
+    return (
+        <Box paddingX="10px" paddingY={["5px", "2px", "2px", "0px"]} marginRight={props.marginRight} width={["100%", "100%", "auto", "auto"]}>
+            <Link to={props.to}>{props.children}</Link>
+        </Box>
+    );
+};
 
 const Links = () => {
     return (
         <>
             <Box as="nav" margin="20px">
-                <List textAlign="end">
-                    <ListItem float="left" padding="0 10px" fontFamily="Lora" fontSize="2em"><Link to="/">Learn Thai Style</Link></ListItem>
-                    <ItemRender to="/tests">Test</ItemRender>
-                    <ItemRender to="/learn">Learn</ItemRender>
-                    <ItemRender to="/about">About Us</ItemRender>
-                    <ItemRender to="/login">
-                        <Button variantColor="teal" borderRadius="16px">Get Started</Button>
-                    </ItemRender>
-                </List>
+                <Grid gridTemplateColumns="1fr 1fr" gridTemplateRows="1fr">
+                    <Box
+                        float="left"
+                        padding="0 10px"
+                        fontFamily="Lora"
+                        fontSize="2em"
+                    >
+                        <Link to="/">Learn Thai Style</Link>
+                    </Box>
+                    <Stack textAlign="right" flexDir={["column","column","row","row"]} justifySelf="right" alignItems="center" flexShrink={1}>
+                        <ItemRender to="/tests" marginRight={["20px", "20px", "0px", "0px"]}>Test</ItemRender>
+                        <ItemRender to="/learn" marginRight={["20px", "20px", "0px", "0px"]}>Learn</ItemRender>
+                        <ItemRender to="/about" marginRight={["20px", "20px", "0px", "0px"]}>About Us</ItemRender>
+                        <ItemRender to="/login">
+                            <Button variantColor="teal" borderRadius="16px">
+                                Get Started
+                            </Button>
+                        </ItemRender>
+                    </Stack>
+                </Grid>
             </Box>
         </>
     );
